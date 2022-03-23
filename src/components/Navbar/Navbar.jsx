17396904bab539e0be7fb5fa3f3ff85d2c371d2b
Hum Bruno <hsbruno1@gmail.com>
2022-03-23 21:12:32 +0000

    navbar responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,46 @@
 import styles from "./styles.module.scss";
+import { FaBars, FaTimes, FaCircle } from "react-icons/fa";
+import { useState } from "react";
 
 const Navbar = ({ planets }) => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
   return (
     <header className={styles.nav}>
       <a href="#">
         <h1>The Planets</h1>
       </a>
-      <ul>
+      <ul
+        className={
+          click ? `${styles.navMenu} ${styles.active}` : styles.navMenu
+        }
+      >
         {planets.map((planet) => {
           return (
             <a href="#">
-              <li>{planet.name}</li>
+              <li>
+                <span>
+                  <FaCircle
+                    className={styles.planetIcon}
+                    size={20}
+                    style={{ color: planet.color }}
+                  />
+                </span>
+                {planet.name}
+              </li>
             </a>
           );
         })}
       </ul>
+
+      <div className={styles.hamburgerMenu} onClick={handleClick}>
+        {click ? (
+          <FaTimes size={24} style={{ color: "#fff" }} />
+        ) : (
+          <FaBars size={24} style={{ color: "#fff" }} />
+        )}
+      </div>
     </header>
   );
 };
